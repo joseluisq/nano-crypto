0cb4d89dc24da1f32848080ed342f03439c48537
Jose Quintana <joseluisquintana20@gmail.com>
2020-02-17 23:10:22 +0100

    feat: crypto.browser aes function

--- src/crypto.browser.ts
+++ src/crypto.browser.ts
@@ -1,13 +1,9 @@
 type CryptoDigestAlgorithm = "SHA-256" | "SHA-384" | "SHA-512"
+type CryptoAesAlgoParams = AesCtrParams | AesCbcParams | AesGcmParams
 
 const CRYPTO_ENCRYPT_ALGO = "AES-GCM"
 const CRYPTO_ENCRYPT_ALGO_LEN = 256
 const CRYPTO_ENCRYPT_HASH_ALGO = "SHA-256"
-
-function arrayBufferToHex (buf: ArrayBuffer) {
-    const hashArray = Array.from(new Uint8Array(buf))
-    return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
-}
 
 export async function hash (data: Uint8Array, algorithm: CryptoDigestAlgorithm) {
     return window.crypto.subtle.digest(algorithm, data)
@@ -22,84 +18,95 @@ export function digest (algorithm: CryptoDigestAlgorithm) {
     }
 }
 
-/**
- * Generates an `RSA-OAEP` encryption key pair.
- * Note: Consider using a 4096-bit key (modulusLength) for systems that require long-term security
- *
- * @param modulusLength The length in bits of the RSA modulus. This should be at least 2048. Default 2048.
- * @param hash A DOMString representing the name of the digest function to use.
- * It can pass any of SHA-256, SHA-384, or SHA-512. Default SHA-256.
- */
-function generateRsaKey (modulusLength = 2048, hash = CRYPTO_ENCRYPT_HASH_ALGO) {
-    return window.crypto.subtle.generateKey({
-        name: "RSA-OAEP",
-        publicExponent: new Uint8Array([ 1, 0, 1 ]),
-        modulusLength,
-        hash
-    },
-    true,
-    [ "encrypt", "decrypt" ])
+export function arrayBufferToHex (buf: ArrayBuffer) {
+    const hashArray = Array.from(new Uint8Array(buf))
+    return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
 }
 
-/**
- * Generates an `AES-GCM` encryption key pair.
- *
- * @param length The length in bits of the key to generate. This must be 128, 192 or 256. Default 256.
- */
-function generateAesKey (length = CRYPTO_ENCRYPT_ALGO_LEN, hash = CRYPTO_ENCRYPT_HASH_ALGO) {
-    return window.crypto.subtle.generateKey({
-        name: CRYPTO_ENCRYPT_ALGO,
-        length,
-        hash
-    },
-    true,
-    [ "encrypt", "decrypt" ])
-}
+export const createIV = (length = 16) => window.crypto.getRandomValues(new Uint8Array(length))
 
-export function encrypt (
-    data: Uint8Array,
-    algorithm: string | RsaOaepParams | AesCtrParams | AesCbcParams | AesCmacParams | AesGcmParams | AesCfbParams,
-    publicKey: CryptoKey
-) {
-    return Promise.resolve(window.crypto.subtle.encrypt(algorithm, publicKey, data))
-}
+export function AES (algorithm: CryptoAesAlgoParams) {
+    const createGcmAlgoParams = () => ({ name: CRYPTO_ENCRYPT_ALGO, iv: createIV() } as AesGcmParams)
 
-export function cypher (
-    algorithm?: string | RsaOaepParams | AesCtrParams | AesCbcParams | AesCmacParams | AesGcmParams | AesCfbParams
-) {
-    function defaultAesGcmParams () {
-        // The iv must never be reused with a given key.
-        const iv = window.crypto.getRandomValues(new Uint8Array(12))
+    async function encryptFn (data: string | Uint8Array, key: CryptoKey, algo: CryptoAesAlgoParams) {
+        return new Promise<[ArrayBuffer, Uint8Array]>(async (resolve, reject) => {
+            if (!data) {
+                return reject(new TypeError("Data to encrypt not supplied"))
+            }
 
-        return {
-            name: CRYPTO_ENCRYPT_ALGO,
-            iv
-        }
+            if (!key) {
+                return reject(new TypeError("The key was not specified"))
+            }
+
+            if (!algo) {
+                return reject(new TypeError("Algorithm was not specified"))
+            }
+
+            let units: Uint8Array | undefined = undefined
+
+            if (typeof data === "string") {
+                units = new window.TextEncoder().encode(data)
+            }
+
+            if (typeof data === "object" && data instanceof Uint8Array) {
+                units = data
+            }
+
+            if (!units) {
+                return reject(new TypeError("Data type to encrypt is not Uint8Array or string"))
+            }
+
+            try {
+                let iv: Uint8Array | undefined = undefined
+
+                if (!algo.hasOwnProperty("name")) {
+                    return reject(new TypeError("Algorithm object doesn't contain property name"))
+                }
+
+                // `AesCbcParams` and `AesGcmParams` have an `iv` prop
+                if (algo.hasOwnProperty("iv")) {
+                    iv = (algo as AesGcmParams).iv as Uint8Array
+                }
+
+                // `AesCtrParams` has only a `counter` prop
+                if (algo.hasOwnProperty("counter")) {
+                    iv = (algo as AesCtrParams).counter as Uint8Array
+                }
+
+                if (!iv) {
+                    return reject(new TypeError("Algorithm object doesn't contain property iv or counter"))
+                }
+
+                const buf = await Promise.resolve(window.crypto.subtle.encrypt(algo, key, units))
+
+                resolve([ buf, iv ])
+            } catch (err) {
+                reject(err)
+            }
+        })
     }
 
-    async function encryptFn (
-        data: string,
-        publicKey: CryptoKey,
-        algo: string | RsaOaepParams | AesCtrParams | AesCbcParams | AesCmacParams | AesGcmParams | AesCfbParams
-    ) {
-        const units = new window.TextEncoder().encode(data)
-        return arrayBufferToHex(await encrypt(units, algo, publicKey))
+    /**
+     * Generates an `AES-GCM` encryption key pair.
+     *
+     * @param length The length in bits of the key to generate. This must be 128, 192 or 256. Default 256.
+     */
+    function createGcmKey (length = CRYPTO_ENCRYPT_ALGO_LEN, hash = CRYPTO_ENCRYPT_HASH_ALGO) {
+        return window.crypto.subtle.generateKey({
+            name: CRYPTO_ENCRYPT_ALGO,
+            length,
+            hash
+        },
+        true,
+        [ "encrypt", "decrypt" ])
     }
 
     return {
-        generateAesKey,
-        generateRsaKey,
+        createGcmKey,
+        createGcmAlgoParams,
 
-        encrypt: (data: string, publicKey: CryptoKey) => {
-            if (!algorithm) {
-                throw new Error("Algorithm was not provided.")
-            }
+        encrypt: (data: string | Uint8Array, key: CryptoKey) => encryptFn(data, key, algorithm),
 
-            return encryptFn(data, publicKey, algorithm)
-        },
-
-        encryptAesGcm: async (data: string, publicKey: CryptoKey) => {
-            return encryptFn(data, publicKey, defaultAesGcmParams())
-        }
+        encryptGcm: (data: string | Uint8Array, key: CryptoKey) => encryptFn(data, key, createGcmAlgoParams())
     }
 }
